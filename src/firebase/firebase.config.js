07c00577b743bba2eb08e95ab5bdc36177a09983
Shamsul Islam Sujon <sujon2762@gmail.com>
2022-10-27 15:06:45 +0600

    Environment variable

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyD_UEeypTcdT-SnrN6qDxSTAidpAclB8LI",
-    authDomain: "zinius-e-learning.firebaseapp.com",
-    projectId: "zinius-e-learning",
-    storageBucket: "zinius-e-learning.appspot.com",
-    messagingSenderId: "529003473377",
-    appId: "1:529003473377:web:68112c8c289d19034c4aa0",
-    measurementId: "G-67X5ME1P92",
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId,
+    measurementId: process.env.REACT_APP_measurementId,
 };
 
 // Initialize Firebase
